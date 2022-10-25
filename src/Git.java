1dc0472ea8ffd530c1f68fb94c9d418fa4bff497
Nico1105709883 <fnpadilla@utpl.edu.ec>
2022-10-25 12:17:35 -0500

    First commit

---
+++ src/Git.java
@@ -0,0 +1,11 @@
+public class Git {
+    public static void main(String[] args) {
+        System.out.printf("'%S'","Comando basicos de GitHub \n \n");
+        System.out.println("Primero se vincula la cuenta de GitHub con IntelliJ Idea, esto se logra con los comandos git config user.name y user.email\n");
+        System.out.println("Usamos el comando git init para crear un nuevo repositorio\n");
+        System.out.println("Usamos el comando git status para conocer los cambios que se deben realizar\n");
+        System.out.println("Usamos git add . para añadir los cambios pendientes\n");
+        System.out.println("Usamos el comando git commit para confirmar los cambios\n");
+        System.out.println("Para finalizar usamos git push cargar contenido del repositorio local a un repositorio remoto.");
+    }
+}
