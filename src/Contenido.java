789b064c6aff4a20d479de550e81a95819fed27c
Nico1105709883 <fnpadilla@utpl.edu.ec>
2022-10-25 11:26:59 -0500

    first commit

---
+++ src/Contenido.java
@@ -0,0 +1,24 @@
+public class Contenido {
+    public static void main(String[] args) {
+        System.out.printf("'%S'","Conceptos basicos de programacion \n \n");
+        System.out.println("Algoritmo: Es un conjunto de pasos para una determinada tarea.\n");
+        System.out.println("Código: Conjunto de palabras o símbolos que contienen instrucciones para la computadora.\n");
+        System.out.println("Código fuente: código de un programa. La computadora lo usa para generar una versión del programa que es capaz de procesar.\n ");
+        System.out.println("Código Máquina: código que la máquina puede entender y ejecutar.\n");
+        System.out.println("Código Binario: código formado solo por 0 y 1 que tiene como función codificar algo. Es la base de toda la lógica de la computadora.\n");
+        System.out.println("Big Data: almacenamiento de grandes cantidades de datos.\n");
+        System.out.println("Bit: abreviatura de binary digit, es decir, representa solo un valor binario (cero o uno).\n");
+        System.out.println("Byte: unión de 8 bits.\n");
+        System.out.println("Bug: problema en el código que hace que no ejecute correctamente su función.\n");
+        System.out.println("Console: ventana, normalmente negra, donde se puede trabajar desde la línea de comandos.\n");
+        System.out.println("Clase: proyecto de todo objeto.\n");
+        System.out.println("Objeto: la ejecución de una clase.\n");
+        System.out.println("Sistema operativo: sistema que se comunica entre el hardware, gestionando y distribuyendo sus recursos, y los demás softwares y crear una plataforma común para todos los programas utilizados.\n");
+        System.out.println("IDE: un editor de texto es una aplicación para editar archivos de texto. Esto incluye códigos de todo tipo que también son textos escritos en un formato que la computadora puede entender.\n");
+        System.out.println("Compilador: transforma el código en un lenguaje de programación a un lenguaje de máquina \n");
+        System.out.println("Interpretador: programas de la computadora que leen un código fuente de un lenguaje de programación interpretado\n");
+        System.out.println("Base de Datos: agrupación (en forma de tablas, documentos, columnas, etc.) de información que se relaciona y trata sobre un mismo tema.\n");
+        System.out.println("Backup: copia de archivos en otro dispositivo para guardar información.\n");
+        System.out.println("Shell: ventana, normalmente negra, para manipular el sistema operativo a través de la línea de comandos.\n");
+        System.out.println("Plugin: un extensión de código que se puede agregar a un código en el que estás trabajando. ");
+    }
